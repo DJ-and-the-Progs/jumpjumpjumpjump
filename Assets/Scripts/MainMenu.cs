7c528fff3ed687f8dd6c707b275acff798624d12
Frankie DiPietro <fad4470@rit.edu>
2016-11-12 12:04:24 -0500

    Add basic main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Buttons called by the buttons themselves
+
+    public void OnPlayButtonClicked()
+    {
+        Application.LoadLevel("game");
+    }
+
+    public void OnSettingsButtonClicked()
+    {
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
