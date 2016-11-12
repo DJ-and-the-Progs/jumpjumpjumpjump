84db59c534afb89aec13e56cdc031281915ff79f
Frankie DiPietro <fad4470@rit.edu>
2016-11-12 01:27:00 -0500

    Add some more enemies

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private int hits = 1;
+
+    private int maxHits;
+
+	// Use this for initialization
+	void Start () {
+        maxHits = hits;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected virtual void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    protected virtual void OnPlayerBounceOn(GameObject player)
+    {
+        this.hits--;
+        if (this.hits <= 0)
+            this.Die();
+
+        Vector3 scale = this.transform.localScale;
+        scale.y = (float)hits / (float)maxHits;
+        this.transform.localScale = scale;
+    }
+}
