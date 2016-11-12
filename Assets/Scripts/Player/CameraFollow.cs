413884b12c23bae034e0d6b46b24706dfff82686
Jonathan Palmer <vantharion@gmail.com>
2016-11-12 02:56:16 -0500

    camera following is solid

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -3,42 +3,57 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    [SerializeField]
+	[SerializeField]
+	private AnimationCurve fallCurve;
+
+	[SerializeField]
     private Transform target;
+
+	[SerializeField]
+	private float cameraHorizontalFollowSpeed;
 
 	[SerializeField]
 	private float cameraHeightThresholdPercent;
 
-	private Camera mainCamera;
-	private float targetYPosition;
-	private float thresholdDepth;
-	private float screenCenter;
+	[SerializeField]
+	private float cameraRiseSpeed;
+
+	private float cameraAcceleration;
+
+	private float fallingTime;
 
 	// Use this for initialization
 	void Start () {
         Debug.Assert(target != null);
-		mainCamera = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 position = this.transform.position;
-        position.x = Mathf.Lerp(position.x, target.position.x, 0.25f);
 
-		// gets main camera every time incase camera's are changed
-		// stored as instance variable so local variable space does not need to be reallocated on every update
-		mainCamera = Camera.main;
+		//position.x = Mathf.Lerp(position.x, target.position.x, 0.0005f);
+		// switched to moveTorwards because has a cool mvoing affect
+		position.x = Mathf.MoveTowards(position.x, target.position.x, cameraHorizontalFollowSpeed);
 
-		targetYPosition = mainCamera.WorldToScreenPoint(target.position).y;
+		float targetYPosition = Camera.main.WorldToScreenPoint(target.position).y;
 
 		// calculates threshold depth for character position to not exceed from center
-		screenCenter = Screen.height / 2;
-		thresholdDepth = Screen.height - Screen.height * cameraHeightThresholdPercent;
-		if (targetYPosition > screenCenter + thresholdDepth || targetYPosition < screenCenter - thresholdDepth)
+		float screenCenter = Screen.height / 2;
+		float thresholdDepth = Screen.height - Screen.height * cameraHeightThresholdPercent;
+
+		//needs to calculate new camera acceleration if falling
+		fallingTime = (targetYPosition < screenCenter - thresholdDepth) ? fallingTime + Time.deltaTime : 0;
+		cameraAcceleration = (targetYPosition < screenCenter - thresholdDepth) ? cameraAcceleration+fallCurve.Evaluate(fallingTime) : 0;
+
+		if (targetYPosition > screenCenter + thresholdDepth)
 		{
-			position.y += target.position.y*0.05f;
+			position.y = Mathf.MoveTowards(position.y, target.position.y, cameraRiseSpeed);
+		}
+		else if (targetYPosition < screenCenter - thresholdDepth)
+		{
+			position.y = Mathf.MoveTowards(position.y, target.position.y, cameraAcceleration);
 		}
 
-		this.transform.position = position;
+			this.transform.position = position;
 	}
 }
