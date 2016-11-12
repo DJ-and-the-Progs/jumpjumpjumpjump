2c93252348f970f429a2418e60f37b8e339e361b
Jonathan Palmer <vantharion@gmail.com>
2016-11-12 01:37:00 -0500

    changes y camera height badly

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -6,9 +6,18 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField]
     private Transform target;
 
+	[SerializeField]
+	private float cameraHeightThresholdPercent;
+
+	private Camera mainCamera;
+	private float targetYPosition;
+	private float thresholdDepth;
+	private float screenCenter;
+
 	// Use this for initialization
 	void Start () {
         Debug.Assert(target != null);
+		mainCamera = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +25,20 @@ public class CameraFollow : MonoBehaviour {
         Vector3 position = this.transform.position;
         position.x = Mathf.Lerp(position.x, target.position.x, 0.25f);
 
-        this.transform.position = position;
+		// gets main camera every time incase camera's are changed
+		// stored as instance variable so local variable space does not need to be reallocated on every update
+		mainCamera = Camera.main;
+
+		targetYPosition = mainCamera.WorldToScreenPoint(target.position).y;
+
+		// calculates threshold depth for character position to not exceed from center
+		screenCenter = Screen.height / 2;
+		thresholdDepth = Screen.height - Screen.height * cameraHeightThresholdPercent;
+		if (targetYPosition > screenCenter + thresholdDepth || targetYPosition < screenCenter - thresholdDepth)
+		{
+			position.y += target.position.y*0.05f;
+		}
+
+		this.transform.position = position;
 	}
 }
